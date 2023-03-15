f3867345d7733d3b8fa53c11c15f6d740fce3fa4
lautii <lautaro.olmos@clovinn.com>
2023-03-15 20:07:56 -0300

    home component and styles

--- app/page.js
+++ app/page.js
@@ -1,14 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import styles from "./page.module.css";
+import Link from 'next/link';
+import { BsLinkedin, BsGithub, BsInstagram} from "react-icons/bs";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to my website</h1>
-      <p>Here you can find information about me and my work.</p>
-      </>
+    <div className={styles.container}>
+      <div className={styles.box}>
+      <h3>Hello <span >I'am </span></h3>
+      <h1>Lautaro Olmos</h1>
+      <h2> Software Developer </h2>
+      <div style={{paddingTop: 30, paddingBottom: 60}}>
+      <p>I'm a web and application developer experienced in designing and developing custom solutions for a variety of industries and clients. In my portfolio, you'll find a selection of projects that showcase my skills in web and application design and development, as well as my ability to collaborate with clients to meet their specific needs.</p>
+    </div>
+    <p>Check out my: </p>
+    <div className={styles.column}>
+    <Link className={styles.divider} href="https://www.linkedin.com/in/lautaro-olmos-dev/" ><BsLinkedin size={25} /></Link>
+    <Link className={styles.divider} href="https://github.com/OlmosLautaroGaston" ><BsGithub size={25} /></Link>
+    <Link className={styles.divider} href="https://www.instagram.com/lautaolmos/" ><BsInstagram size={25} /></Link>
+    </div>
+    </div>
+    </div>
   )
 }
--- app/skills/page.js
+++ app/skills/page.js
@@ -1,7 +1,15 @@
-export default function skills () {
+import styles from "../page.module.css";
+
+async function getSkills() {
+    const skills = await fetch("/api/skills");
+    return skills.json();
+    
+}
+
+export default async function skills () {
     return (
-        <div className="skills">
-            <h1>skills</h1>
-        </div>
+        <div className={styles.container}>
+      
+    </div>
         )
 }
